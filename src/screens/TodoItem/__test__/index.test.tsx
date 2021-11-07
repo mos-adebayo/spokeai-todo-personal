eee811ed32c53c1d5d0866c52f3fe533009e1983
Moses Adebayo <mos.adebayo@gmail.com>
2021-11-07 22:10:29 +0100

    Todo item screen test

--- src/screens/TodoItem/__test__/index.test.tsx
+++ src/screens/TodoItem/__test__/index.test.tsx
@@ -1,18 +1,67 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { shallow } from "enzyme";
+import {mount, ReactWrapper} from "enzyme";
 import TodoItem from "../index";
 import configureStore from "redux-mock-store";
+import {todo} from "../../../__fixtures/todo";
+import * as redux from "react-redux";
+import {BrowserRouter} from "react-router-dom";
 
 const mockStore = configureStore();
+const mockState = {
+  task: todo ,
+  error: { message: "" }
+};
+
+jest.mock("axios", () => {
+  return {
+    get: () => ({
+      data : {
+        id: "1234567890",
+        title: "Title",
+        actionItems: [
+          {
+            isDone: true,
+            description: "description 1"
+          },
+          {
+            isDone: false,
+            description: "description 2"
+          },
+          {
+            isDone: false,
+            description: "description 3"
+          }
+        ]
+      }
+    })
+  };
+});
+
 
 describe("<TodoItem />", () => {
-  it("should match snapshot for form creation", () => {
-    const wrapper = shallow(
-      <Provider store={mockStore()}>
-        <TodoItem />
-      </Provider>
+  let wrapper: ReactWrapper;
+  let spyOnUseSelector;
+  let spyOnUseDispatch;
+  const mockDispatch = jest.fn();
+
+  beforeEach(() => {
+    spyOnUseSelector = jest.spyOn(redux, "useSelector");
+    spyOnUseSelector.mockReturnValue(mockState);
+
+    spyOnUseDispatch = jest.spyOn(redux, "useDispatch");
+    spyOnUseDispatch.mockReturnValue(mockDispatch);
+
+    wrapper = mount(
+        <Provider store={mockStore()}>
+          <BrowserRouter>
+            <TodoItem />
+          </BrowserRouter>
+        </Provider>
     );
+  });
+
+  it("should match snapshot for form creation", () => {
     expect(wrapper).toMatchSnapshot();
   });
 });
